41b7b2985e6d43ef515ff59966ab777d39f6873b
Marcin Pietrzak <marcin@iworks.pl>
2016-01-28 23:15:46 +0100

    Added draft of plugin settings page. Added ability to activate plugin in "Network Activation".

--- vendor/iworks/manage-inactive-subsites.php
+++ vendor/iworks/manage-inactive-subsites.php
@@ -31,20 +31,40 @@ if ( class_exists( 'IworksManageInactiveSubsites' ) ) {
  *
  * Description.
  *
- * @since x.x.x
+ * @since 1.0.0
  */
 class IworksManageInactiveSubsites {
+
     /**
-     * Summary.
+     * Plugin base name
      *
-     * @since x.x.x
-     * @access (private, protected, or public)
-     * @var type $var Description.
+     * @since 1.0.0
+     * @access private
+     * @var string $base plugin base name
+     */
+    private $base_name;
+
+    /**
+     * Plugin version.
+     *
+     * @since 1.0.0
+     * @access private
+     * @var string $version plugin version
      */
     private $version = 'trunk';
 
     public function __construct() {
+        /**
+         * static settings
+         */
+        $this->plugin_basename = plugin_basename( dirname( dirname( dirname( __FILE__ ) ) ) ) . '/manage-inactive-subsites.php';
+
+        /**
+         * actions
+         */
         add_action( 'admin_init', array( $this,'deactivate_plugin_for_non_network_install' ) );
+        add_action( 'network_admin_menu', array( $this, 'add_network_settings_submenu' ) );
+        add_action( 'admin_menu', array( $this, 'add_network_settings_submenu' ) );
     }
 
     /**
@@ -52,7 +72,51 @@ class IworksManageInactiveSubsites {
      *
      * Description.
      *
-     * @since x.x.x
+     * @since 1.0.0
+     * @access (for functions: only use if private)
+     *
+     * @see Function/method/class relied on
+     * @link URL
+     * @global type $varname Description.
+     * @global type $varname Description.
+     *
+     * @param type $var Description.
+     * @param type $var Optional. Description.
+     * @return type Description.
+     */
+    public function add_network_settings_submenu() {
+        add_submenu_page(
+            'settings.php',
+            __('Manage Inactive Subsites', 'manage-inactive-subsites'),
+            __('Manage Inactive Subsites', 'manage-inactive-subsites'),
+            'manage_network_options',
+            'manage-inactive-subsites-settings',
+            array($this, 'plugin_settings_page')
+        );
+
+    }
+
+    /**
+     * Manage Inactive Subsites settings page
+     *
+     * This function produce plugin settings page, which allow to setup plugin
+     * behaviors.
+     *
+     * @since 1.0.0
+     *
+     */
+    public function plugin_settings_page() {
+        echo '<div class="wrap">';
+        printf( '<h1>%s</h1>', __( 'Manage Inactive Subsites', 'manage-inactive-subsites' ) );
+        echo '</div>';
+    }
+
+    /**
+     * Summary.
+     *
+     * Description.
+     *
+     * @since 1.0.0
      * @access (for functions: only use if private)
      *
      * @see Function/method/class relied on
@@ -65,11 +129,21 @@ class IworksManageInactiveSubsites {
      * @return type Description.
      */
     public function deactivate_plugin_for_non_network_install() {
+        if ( is_network_admin() ) {
+            /**
+             * delete from main site, to avoid problems
+             */
+            if ( is_main_site() ) {
+                delete_option('manage-inactive-subsites-deactivate');
+            }
+            return;
+        }
         if ( 'deactivate' != get_option( 'manage-inactive-subsites-deactivate', '' ) ) {
             return;
         }
         add_action( 'admin_notices', array( $this, 'deactivate_plugin_for_non_network_install_admin_notice' ) );
-        deactivate_plugins( plugin_basename( dirname( dirname( dirname( __FILE__ ) ) ) ) . '/manage-inactive-subsites.php' );
+        deactivate_plugins( $this->plugin_basename );
+        delete_option('manage-inactive-subsites-deactivate');
     }
 
     /**
@@ -77,7 +151,7 @@ class IworksManageInactiveSubsites {
      *
      * Description.
      *
-     * @since x.x.x
+     * @since 1.0.0
      * @access (for functions: only use if private)
      *
      * @see Function/method/class relied on
@@ -95,4 +169,5 @@ class IworksManageInactiveSubsites {
             __('<b>Manage Inactive Subsites</b> can be only instaled as network activation.', 'manage-inactive-subsites')
         );
     }
+
 }
